9faf26ce599cbe74c519ad2ba6e66d1257d95d77
Xavier Garrido <xavier.garrido@gmail.com>
2020-01-03 18:38:20 +0100

    fix

--- setup.py
+++ setup.py
@@ -29,8 +29,8 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6"
-        "Programming Language :: Python :: 3.7"
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8"
         ],
     entry_points={},
