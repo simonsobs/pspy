6fd02f7c585d02d89ecae873da5ac5d9bdcf492e
Xavier Garrido <xavier.garrido@gmail.com>
2020-01-06 14:52:19 +0100

    pepify so_window

--- pspy/so_window.py
+++ pspy/so_window.py
@@ -1,202 +1,199 @@
 """
-routines for window function generation
+Routines for window function generation
 """
 
-import healpy as hp, pylab as plt, numpy as np, astropy.io.fits as pyfits
-from pixell import enmap,curvedsky
+import healpy as hp
+import numpy as np
+from pixell import curvedsky, enmap
+from scipy.ndimage import distance_transform_edt
+
 from pspy import sph_tools
-import scipy
-import os, sys
+
 
 def get_distance(binary):
-    
-    """Get the distance to the closest masked pixels for CAR and healpix so_map binary.
-        
+    """Get the distance to the closest masked pixels for CAR and HEALPIX ``so_map`` binary.
+
     Parameters
     ----------
-    binary: so_map
-      a so_map with binary data (1 is observed, 0 is masked)
+    binary: ``so_map``
+      a ``so_map`` with binary data (1 is observed, 0 is masked)
     """
-    
-    dist=binary.copy()
-    if binary.pixel=='HEALPIX':
-        dist.data= enmap.distance_transform_healpix(binary.data, method="heap")
-        dist.data*=180/np.pi
-    if binary.pixel=='CAR':
-        pixSize_arcmin= np.sqrt(binary.data.pixsize()*(60*180/np.pi)**2)
-        dist.data[:]= scipy.ndimage.distance_transform_edt(binary.data)
-        dist.data[:]*=pixSize_arcmin/60
+
+    dist = binary.copy()
+    if binary.pixel == "HEALPIX":
+        dist.data = enmap.distance_transform_healpix(binary.data, method="heap")
+        dist.data *= 180 / np.pi
+    if binary.pixel == "CAR":
+        pix_size_arcmin = np.sqrt(binary.data.pixsize() * (60 * 180 / np.pi)**2)
+        dist.data[:] = distance_transform_edt(binary.data)
+        dist.data[:] *= pix_size_arcmin / 60
 
     return dist
 
+
 def create_apodization(binary, apo_type, apo_radius_degree):
-    
     """Create a apodized window function from a binary mask.
-    
+
     Parameters
     ----------
-    binary: so_map
-      a so_map with binary data (1 is observed, 0 is masked)
+    binary: ``so_map``
+      a ``so_map`` with binary data (1 is observed, 0 is masked)
     apo_type: string
       the type of apodisation you want to use ("C1","C2" or "Rectangle")
     apo_radius: float
       the apodisation radius in degrees
     """
 
-    if apo_type=='C1':
-        window=apod_C1(binary,apo_radius_degree)
-    if apo_type=='C2':
-        window=apod_C2(binary,apo_radius_degree)
-    if apo_type=='Rectangle':
-        if binary.pixel=='HEALPIX':
-            print( 'no rectangle apod for healpix map')
-            sys.exit()
-        if binary.pixel=='CAR':
-            window= apod_rectangle(binary,apo_radius_degree)
+    if apo_type == "C1":
+        window = apod_C1(binary, apo_radius_degree)
+    if apo_type == "C2":
+        window = apod_C2(binary, apo_radius_degree)
+    if apo_type == "Rectangle":
+        if binary.pixel == "HEALPIX":
+            raise ValueError("No rectangle apodization for HEALPIX map")
+        if binary.pixel == "CAR":
+            window = apod_rectangle(binary, apo_radius_degree)
 
     return window
 
-def apod_C2(binary,radius):
-    
+
+def apod_C2(binary, radius):
     """Create a C2 apodisation as defined in https://arxiv.org/pdf/0903.2350.pdf
-        
+
     Parameters
     ----------
-    binary: so_map
-        a so_map with binary data (1 is observed, 0 is masked)
+    binary: ``so_map``
+        a ``so_map`` with binary data (1 is observed, 0 is masked)
     apo_radius: float
         the apodisation radius in degrees
 
     """
-    
-    if radius==0:
-        return binary
-    else:
-        dist=get_distance(binary)
-        win=binary.copy()
-        id=np.where(dist.data> radius)
-        win.data=dist.data/radius-np.sin(2*np.pi*dist.data/radius)/(2*np.pi)
-        win.data[id]=1
-        
-    return(win)
 
-def apod_C1(binary,radius):
-    
+    if radius == 0:
+        return binary
+
+    dist = get_distance(binary)
+    idx = np.where(dist.data > radius)
+    win = binary.copy()
+    win.data = dist.data / radius - np.sin(2 * np.pi * dist.data / radius) / (2 * np.pi)
+    win.data[idx] = 1
+
+    return win
+
+
+def apod_C1(binary, radius):
     """Create a C1 apodisation as defined in https://arxiv.org/pdf/0903.2350.pdf
-        
+
     Parameters
     ----------
-    binary: so_map
-      a so_map with binary data (1 is observed, 0 is masked)
+    binary: ``so_map``
+      a ``so_map`` with binary data (1 is observed, 0 is masked)
     apo_radius: float
       the apodisation radius in degrees
-        
-    """
-    
-    if radius==0:
-        return binary
-    else:
-        dist=get_distance(binary)
-        win=binary.copy()
-        id=np.where(dist.data> radius)
-        win.data=1./2-1./2*np.cos(-np.pi*dist.data/radius)
-        win.data[id]=1
-    
-    return(win)
 
-def apod_rectangle(binary,radius):
-    
+    """
+
+    if radius == 0:
+        return binary
+
+    dist = get_distance(binary)
+    idx = np.where(dist.data > radius)
+    win = binary.copy()
+    win.data = 1. / 2 - 1. / 2 * np.cos(-np.pi * dist.data / radius)
+    win.data[idx] = 1
+
+    return win
+
+
+def apod_rectangle(binary, radius):
     """Create an apodisation able for rectangle window (in CAR) (smoother at the corner)
-        
+
     Parameters
     ----------
-    binary: so_map
-      a so_map with binary data (1 is observed, 0 is masked)
+    binary: ``so_map``
+      a ``so_map`` with binary data (1 is observed, 0 is masked)
     apo_radius: float
       the apodisation radius in degrees
-        
+
     """
-    
-    #TODO: clean this one
-    
-    if radius==0:
+
+    if radius == 0:
         return binary
-    else:
-        shape= binary.data.shape
-        wcs= binary.data.wcs
-        Ny,Nx=shape
-        pixScaleY,pixScaleX= enmap.pixshape(shape, wcs)
-        win=binary.copy()
-        win.data= win.data*0+1
-        winX=win.copy()
-        winY=win.copy()
-        Id=np.ones((Ny,Nx))
-        degToPix_x=np.pi/180/pixScaleX
-        degToPix_y=np.pi/180/pixScaleY
-        lenApod_x=int(radius*degToPix_x)
-        lenApod_y=int(radius*degToPix_y)
-    
-        for i in range(lenApod_x):
-            r=float(i)
-            winX.data[:,i]=1./2*(Id[:,i]-np.cos(-np.pi*r/lenApod_x))
-            winX.data[:,Nx-i-1]=winX.data[:,i]
-        for j in range(lenApod_y):
-            r=float(j)
-            winY.data[j,:]=1./2*(Id[j,:]-np.cos(-np.pi*r/lenApod_y))
-            winY.data[Ny-j-1,:]=winY.data[j,:]
 
-        win.data=winX.data*winY.data
+    wcs = binary.data.wcs
+    win = binary.copy()
+    win.data = win.data * 0 + 1
+    win_x = win.copy()
+    win_y = win.copy()
+    shape = binary.data.shape
+    pix_scale_y, pix_scale_x = enmap.pixshape(shape, wcs)
+    deg_to_pix_x = np.pi / 180 / pix_scale_x
+    deg_to_pix_y = np.pi / 180 / pix_scale_y
+    len_apod_x = int(radius * deg_to_pix_x)
+    len_apod_y = int(radius * deg_to_pix_y)
 
-        return(win)
+    ones = np.ones(shape)
+    ny, nx = shape
 
-def get_spinned_windows(w,lmax,niter):
-    
+    for i in range(len_apod_x):
+        r = float(i)
+        win_x.data[:, i] = 1. / 2 * (ones[:, i] - np.cos(-np.pi * r / len_apod_x))
+        win_x.data[:, nx - i - 1] = win_x.data[:, i]
+    for i in range(len_apod_y):
+        r = float(i)
+        win_y.data[i, :] = 1. / 2 * (ones[i, :] - np.cos(-np.pi * r / len_apod_y))
+        win_y.data[ny - i - 1, :] = win_y.data[i, :]
+
+    win.data = win_x.data * win_y.data
+
+    return win
+
+
+def get_spinned_windows(window, lmax, niter):
     """Compute the spinned window functions (for pure B modes method)
-        
+
     Parameters
     ----------
-    w: so_map
+    window: ``so_map``
       map of the window function
     lmax: integer
       maximum value of the multipole for the harmonic transform
     niter: integer
       number of iteration for the harmonic transform
-      
+
     """
 
-    template=np.array([w.data.copy(),w.data.copy()])
-    s1_a,s1_b,s2_a,s2_b=w.copy(),w.copy(),w.copy(),w.copy()
-    
-    if w.pixel=='CAR':
-        template=enmap.samewcs(template,w.data)
-    
-    wlm = sph_tools.map2alm(w,lmax=lmax,niter=niter)
-    ell = np.arange(lmax)
-    filter_1=-np.sqrt((ell+1)*ell)
-    filter_2=-np.sqrt((ell+2)*(ell+1)*ell*(ell-1))
+    template = np.array([window.data.copy(), window.data.copy()])
+    s1_a, s1_b, s2_a, s2_b = window.copy(), window.copy(), window.copy(), window.copy()
 
-    filter_1[:1]=0
-    filter_2[:2]=0
-    wlm1_e= hp.almxfl(wlm,filter_1)
-    wlm2_e= hp.almxfl(wlm,filter_2)
+    if window.pixel == "CAR":
+        template = enmap.samewcs(template, window.data)
+
+    wlm = sph_tools.map2alm(window, lmax=lmax, niter=niter)
+    ell = np.arange(lmax)
+    filter_1 = -np.sqrt((ell + 1) * ell)
+    filter_2 = -np.sqrt((ell + 2) * (ell + 1) * ell * (ell - 1))
+
+    filter_1[:1] = 0
+    filter_2[:2] = 0
+    wlm1_e = hp.almxfl(wlm, filter_1)
+    wlm2_e = hp.almxfl(wlm, filter_2)
     wlm1_b = np.zeros_like(wlm1_e)
     wlm2_b = np.zeros_like(wlm2_e)
 
-    w1=template.copy()
-    w2=template.copy()
-    
-    if w.pixel=='HEALPIX':
-        curvedsky.alm2map_healpix( np.array([wlm1_e,wlm1_b]), w1, spin=1)
-        curvedsky.alm2map_healpix( np.array([wlm2_e,wlm2_b]), w2, spin=2)
-    if w.pixel=='CAR':
-        curvedsky.alm2map(np.array([wlm1_e,wlm1_b]), w1, spin=1)
-        curvedsky.alm2map(np.array([wlm2_e,wlm2_b]), w2, spin=2)
-    
-    s1_a.data[w.data != 0]=w1[0][w.data != 0]
-    s1_b.data[w.data != 0]=w1[1][w.data != 0]
-    s2_a.data[w.data != 0]=w2[0][w.data != 0]
-    s2_b.data[w.data != 0]=w2[1][w.data != 0]
-    
-    return s1_a,s1_b,s2_a,s2_b
+    window1 = template.copy()
+    window2 = template.copy()
 
+    if window.pixel == "HEALPIX":
+        curvedsky.alm2map_healpix(np.array([wlm1_e, wlm1_b]), window1, spin=1)
+        curvedsky.alm2map_healpix(np.array([wlm2_e, wlm2_b]), window2, spin=2)
+    if window.pixel == "CAR":
+        curvedsky.alm2map(np.array([wlm1_e, wlm1_b]), window1, spin=1)
+        curvedsky.alm2map(np.array([wlm2_e, wlm2_b]), window2, spin=2)
 
+    s1_a.data[window.data != 0] = window1[0][window.data != 0]
+    s1_b.data[window.data != 0] = window1[1][window.data != 0]
+    s2_a.data[window.data != 0] = window2[0][window.data != 0]
+    s2_b.data[window.data != 0] = window2[1][window.data != 0]
+
+    return s1_a, s1_b, s2_a, s2_b
