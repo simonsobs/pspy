3aea53a314a7c8a615176eca224cdc6862224cd8
Thibaut <thbtlouis@gmail.com>
2020-01-08 10:42:57 +0100

    more pep8

--- pspy/so_mcm.py
+++ pspy/so_mcm.py
@@ -80,27 +80,27 @@ def mcm_and_bbl_spin0(win1,
     if bl2 is None:
         bl2 = bl1.copy()
 
-    mcm = np.zeros((maxl,maxl))
-    mcm_fortran.calc_mcm_spin0(wcl, bl1*bl2,mcm.T)
-    mcm = mcm[:lmax,:lmax]
-    bin_lo,bin_hi,bin_c,bin_size = pspy_utils.read_binning_file(binning_file,lmax)
+    mcm = np.zeros((maxl, maxl))
+    mcm_fortran.calc_mcm_spin0(wcl, bl1*bl2, mcm.T)
+    mcm = mcm[:lmax, :lmax]
+    bin_lo, bin_hi, bin_c, bin_size = pspy_utils.read_binning_file(binning_file, lmax)
     n_bins = len(bin_hi)
-    mbb = np.zeros((n_bins,n_bins))
-    mcm_fortran.bin_mcm(mcm.T, bin_lo,bin_hi,bin_size, mbb.T,doDl)
+    mbb = np.zeros((n_bins, n_bins))
+    mcm_fortran.bin_mcm(mcm.T, bin_lo, bin_hi, bin_size, mbb.T, doDl)
 
-    Bbl = np.zeros((n_bins,lmax))
-    mcm_fortran.binning_matrix(mcm.T,bin_lo,bin_hi,bin_size, Bbl.T,doDl)
+    Bbl = np.zeros((n_bins, lmax))
+    mcm_fortran.binning_matrix(mcm.T, bin_lo, bin_hi, bin_size, Bbl.T, doDl)
     mbb_inv = np.linalg.inv(mbb)
-    Bbl = np.dot(mbb_inv,Bbl)
+    Bbl = np.dot(mbb_inv, Bbl)
 
     if unbin:
         mcm_inv = np.linalg.inv(mcm)
         if save_file is not None:
-            save_coupling(save_file,mbb_inv,Bbl,mcm_inv=mcm_inv)
-        return mcm_inv,mbb_inv,Bbl
+            save_coupling(save_file, mbb_inv, Bbl, mcm_inv=mcm_inv)
+        return mcm_inv, mbb_inv, Bbl
     else:
         if save_file is not None:
-            save_coupling(save_file,mbb_inv,Bbl)
+            save_coupling(save_file, mbb_inv, Bbl)
         return mbb_inv, Bbl
 
 def mcm_and_bbl_spin0and2(win1,
@@ -149,7 +149,7 @@ def mcm_and_bbl_spin0and2(win1,
     """
     
     def get_coupling_dict(array,fac=1.0):
-        ncomp,dim1,dim2 = array.shape
+        ncomp, dim1, dim2 = array.shape
         dict={}
         dict["spin0xspin0"] = array[0, :, :]
         dict["spin0xspin2"] = array[1, :, :]
@@ -164,18 +164,18 @@ def mcm_and_bbl_spin0and2(win1,
         return dict
     
     if type == "Dl":
-        doDl=1
+        doDl = 1
     if type == "Cl":
-        doDl=0
+        doDl = 0
 
     maxl = lmax
     if lmax_pad is not None:
         maxl = lmax_pad
 
     if input_alm == False:
-        win1 = (sph_tools.map2alm(win1[0],niter=niter,lmax=maxl), sph_tools.map2alm(win1[1],niter=niter,lmax=maxl))
+        win1 = (sph_tools.map2alm(win1[0],niter=niter,lmax=maxl), sph_tools.map2alm(win1[1], niter=niter, lmax=maxl))
         if win2 is not None:
-            win2 = (sph_tools.map2alm(win2[0],niter=niter,lmax=maxl), sph_tools.map2alm(win2[1],niter=niter,lmax=maxl))
+            win2 = (sph_tools.map2alm(win2[0],niter=niter,lmax=maxl), sph_tools.map2alm(win2[1], niter=niter, lmax=maxl))
     if win2 is None:
         win2 = deepcopy(win1)
 
@@ -190,7 +190,7 @@ def mcm_and_bbl_spin0and2(win1,
 
     for i,s1 in enumerate(spin):
         for j,s2 in enumerate(spin):
-            wcl[s1+s2] = hp.alm2cl(win1[i],win2[j])
+            wcl[s1+s2] = hp.alm2cl(win1[i], win2[j])
             #wcl[s1+s2]=wcl[s1+s2][:lmax]*(2*np.arange(lmax)+1)
             wcl[s1+s2] = wcl[s1+s2]*(2*np.arange(len(wcl[s1+s2]))+1)
             wbl[s1+s2] = bl1[i]*bl2[j]
@@ -198,30 +198,34 @@ def mcm_and_bbl_spin0and2(win1,
     mcm = np.zeros((5, maxl, maxl))
 
     if pure==False:
-        mcm_fortran.calc_mcm_spin0and2(wcl["00"],wcl["02"],wcl["20"],wcl["22"], wbl["00"],wbl["02"],wbl["20"], wbl["22"],mcm.T)
+        mcm_fortran.calc_mcm_spin0and2(wcl["00"], wcl["02"], wcl["20"], wcl["22"],
+                                       wbl["00"], wbl["02"], wbl["20"], wbl["22"],
+                                       mcm.T)
     else:
-        mcm_fortran.calc_mcm_spin0and2_pure(wcl["00"],wcl["02"],wcl["20"],wcl["22"], wbl["00"],wbl["02"],wbl["20"], wbl["22"],mcm.T)
+        mcm_fortran.calc_mcm_spin0and2_pure(wcl["00"], wcl["02"], wcl["20"], wcl["22"],
+                                            wbl["00"], wbl["02"], wbl["20"], wbl["22"],
+                                            mcm.T)
 
     mcm = mcm[:, :lmax, :lmax]
 
-    bin_lo,bin_hi,bin_c,bin_size = pspy_utils.read_binning_file(binning_file,lmax)
+    bin_lo, bin_hi, bin_c, bin_size = pspy_utils.read_binning_file(binning_file, lmax)
     n_bins = len(bin_hi)
 
-    mbb_array = np.zeros((5,n_bins,n_bins))
-    Bbl_array = np.zeros((5,n_bins,lmax))
+    mbb_array = np.zeros((5, n_bins, n_bins))
+    Bbl_array = np.zeros((5, n_bins, lmax))
 
     for i in range(5):
-        mcm_fortran.bin_mcm((mcm[i,:,:]).T, bin_lo,bin_hi,bin_size, (mbb_array[i,:,:]).T,doDl)
-        mcm_fortran.binning_matrix((mcm[i,:,:]).T,bin_lo,bin_hi,bin_size, (Bbl_array[i,:,:]).T,doDl)
+        mcm_fortran.bin_mcm((mcm[i,:,:]).T, bin_lo, bin_hi, bin_size, (mbb_array[i,:,:]).T, doDl)
+        mcm_fortran.binning_matrix((mcm[i,:,:]).T, bin_lo, bin_hi, bin_size, (Bbl_array[i,:,:]).T, doDl)
 
-    mbb = get_coupling_dict(mbb_array,fac=-1.0)
-    Bbl = get_coupling_dict(Bbl_array,fac=1.0)
+    mbb = get_coupling_dict(mbb_array, fac=-1.0)
+    Bbl = get_coupling_dict(Bbl_array, fac=1.0)
 
     spin_pairs = ["spin0xspin0", "spin0xspin2", "spin2xspin0", "spin2xspin2"]
     mbb_inv = {}
     for s in spin_pairs:
         mbb_inv[s] = np.linalg.inv(mbb[s])
-        Bbl[s] = np.dot(mbb_inv[s],Bbl[s])
+        Bbl[s] = np.dot(mbb_inv[s], Bbl[s])
 
     if unbin:
         mcm = get_coupling_dict(mcm[:, :lmax-2, :lmax-2],fac=-1.0)
@@ -230,12 +234,12 @@ def mcm_and_bbl_spin0and2(win1,
             mcm_inv[s] = np.linalg.inv(mcm[s])
 
         if save_file is not None:
-            save_coupling(save_file,mbb_inv,Bbl,spin_pairs=spin_pairs,mcm_inv=mcm_inv)
-        return mcm_inv,mbb_inv,Bbl
+            save_coupling(save_file, mbb_inv, Bbl, spin_pairs=spin_pairs, mcm_inv=mcm_inv)
+        return mcm_inv, mbb_inv, Bbl
     else:
         if save_file is not None:
-            save_coupling(save_file,mbb_inv,Bbl,spin_pairs=spin_pairs)
-        return mbb_inv,Bbl
+            save_coupling(save_file, mbb_inv, Bbl, spin_pairs=spin_pairs)
+        return mbb_inv, Bbl
 
 def coupling_dict_to_array(dict):
     """Take a mcm or Bbl dictionnary with entries:
@@ -284,7 +288,7 @@ def apply_Bbl(Bbl,ps,spectra=None):
             ps_vec = np.append(ps_vec, ps[f])
         ps_b = np.dot(Bbl_array,ps_vec)
         n_bins = int(Bbl_array.shape[0]/9)
-        ps_th={}
+        ps_th = {}
         for i,f in enumerate(spectra):
             ps_th[f] = ps_b[i*n_bins:(i+1)*n_bins]
         return ps_th
@@ -339,15 +343,15 @@ def save_coupling(prefix,mbb_inv,Bbl,spin_pairs=None,mcm_inv=None):
 
     if spin_pairs is not None:
         for s in spin_pairs:
-            np.save(prefix +"_mbb_inv_%s.npy"%s,mbb_inv[s])
-            np.save(prefix +"_Bbl_%s.npy"%s,Bbl[s])
+            np.save(prefix +"_mbb_inv_%s.npy"%s, mbb_inv[s])
+            np.save(prefix +"_Bbl_%s.npy"%s, Bbl[s])
             if mcm_inv is not None:
-                np.save(prefix +"_mcm_inv_%s.npy"%s,mcm_inv[s])
+                np.save(prefix +"_mcm_inv_%s.npy"%s, mcm_inv[s])
     else:
-        np.save(prefix +"_mbb_inv.npy",mbb_inv)
-        np.save(prefix +"_Bbl.npy",Bbl)
+        np.save(prefix +"_mbb_inv.npy", mbb_inv)
+        np.save(prefix +"_Bbl.npy", Bbl)
         if mcm_inv is not None:
-            np.save(prefix +"_mcm_inv.npy",mcm_inv)
+            np.save(prefix +"_mcm_inv.npy", mcm_inv)
 
 
 def read_coupling(prefix,spin_pairs=None,unbin=None):
@@ -380,6 +384,6 @@ def read_coupling(prefix,spin_pairs=None,unbin=None):
         Bbl = np.load(prefix +"_Bbl.npy")
 
     if unbin:
-        return mcm_inv,mbb_inv,Bbl
+        return mcm_inv, mbb_inv, Bbl
     else:
-        return mbb_inv,Bbl
+        return mbb_inv, Bbl
