6736759e1b1665c203eccfad1bc706a0f27c62db
Thibaut <thbtlouis@gmail.com>
2020-01-08 09:57:14 +0100

    implement xavier pep8 suggestion

--- pspy/so_spectra.py
+++ pspy/so_spectra.py
@@ -4,8 +4,7 @@ Routines for power spectra estimation and debiasing.
 import healpy as hp, numpy as np
 from pspy import pspy_utils,so_mcm
 
-def get_spectra(alm1,alm2=None,spectra=None):
-    
+def get_spectra(alm1, alm2=None, spectra=None):
     """Get the power spectrum of alm1 and alm2, we use healpy.alm2cl for doing this.
     for the spin0 and spin2 case it is a bit ugly as we have to deal with healpix convention.
     Our  convention for spectra is:  ['TT','TE','TB','ET','BT','EE','EB','BE','BB']
@@ -29,37 +28,36 @@ def get_spectra(alm1,alm2=None,spectra=None):
 
     if spectra is None:
         if alm2 is None:
-            cls=hp.sphtfunc.alm2cl(alm1)
+            cls = hp.sphtfunc.alm2cl(alm1)
         else:
-            cls=hp.sphtfunc.alm2cl(alm1,alm2)
-        l=np.arange(len(cls))
-        return l,cls
+            cls = hp.sphtfunc.alm2cl(alm1,alm2)
+        l = np.arange(len(cls))
+        return l, cls
     else:
-        cl_dict={}
-        cls=hp.sphtfunc.alm2cl(alm1,alm2)
-        l=np.arange(len(cls[0]))
+        cl_dict = {}
+        cls = hp.sphtfunc.alm2cl(alm1,alm2)
+        l = np.arange(len(cls[0]))
         """ spectra_healpix=[TT,EE,BB,TE,EB,TB] """
-        spectra_healpix=[spectra[0],spectra[5],spectra[8],spectra[1],spectra[6],spectra[2]]
+        spectra_healpix = [spectra[0],spectra[5],spectra[8],spectra[1],spectra[6],spectra[2]]
         for c,f in enumerate(spectra_healpix):
-            cl_dict[f]=cls[c]
+            cl_dict[f] = cls[c]
 
         if alm2 is None:
             #here we set ET=TE, BE=EB and BT=TB
-            cl_dict[spectra[3]]= cl_dict[spectra[1]]
-            cl_dict[spectra[7]]= cl_dict[spectra[6]]
-            cl_dict[spectra[4]]= cl_dict[spectra[2]]
+            cl_dict[spectra[3]] = cl_dict[spectra[1]]
+            cl_dict[spectra[7]] = cl_dict[spectra[6]]
+            cl_dict[spectra[4]] = cl_dict[spectra[2]]
         else:
             #here we need to recompute cls inverting the order of the alm to get ET,BT and BE
-            cls=hp.sphtfunc.alm2cl(alm2,alm1)
+            cls = hp.sphtfunc.alm2cl(alm2,alm1)
             """ spectra_healpix=[TT,EE,BB,ET,BE,BT] """
-            spectra_healpix=[spectra[0],spectra[5],spectra[8],spectra[3],spectra[7],spectra[4]]
+            spectra_healpix = [spectra[0],spectra[5],spectra[8],spectra[3],spectra[7],spectra[4]]
             for c,f in enumerate(spectra_healpix):
-                cl_dict[f]=cls[c]
+                cl_dict[f] = cls[c]
 
-        return l,cl_dict
+        return l, cl_dict
 
-def bin_spectra(l,cl,binning_file,lmax,type,spectra=None,mbb_inv=None,mcm_inv=None):
-    
+def bin_spectra(l, cl, binning_file, lmax, type, spectra=None, mbb_inv=None, mcm_inv=None):
     """Bin the power spectra according to a binning file and optionnaly deconvolve the mode coupling matrix
     
     Parameters
@@ -98,33 +96,33 @@ def bin_spectra(l,cl,binning_file,lmax,type,spectra=None,mbb_inv=None,mcm_inv=No
         print ("Error: you have to choose between binned or raw mcm")
         sys.exit()
 
-    bin_lo,bin_hi,bin_c,bin_size= pspy_utils.read_binning_file(binning_file,lmax)
-    n_bins=len(bin_hi)
+    bin_lo, bin_hi, bin_c, bin_size = pspy_utils.read_binning_file(binning_file, lmax)
+    n_bins = len(bin_hi)
     
-    if type=="Dl":
-        fac=(l*(l+1)/(2*np.pi))
-    if type=="Cl":
-        fac=l*0+1 
+    if type == "Dl":
+        fac = (l*(l+1)/(2*np.pi))
+    if type == "Cl":
+        fac = l*0+1
 
     if spectra is None:
         if mcm_inv is not None:
-            cl=np.dot(mcm_inv,cl)
-        binnedPower=np.zeros(len(bin_c))
+            cl = np.dot(mcm_inv,cl)
+        binnedPower = np.zeros(len(bin_c))
         for ibin in range(n_bins):
             loc = np.where((l >= bin_lo[ibin]) & (l <= bin_hi[ibin]))
             binnedPower[ibin] = (cl[loc]*fac[loc]).mean()
         if mbb_inv is None:
-            return bin_c,binnedPower
+            return bin_c, binnedPower
         else:
-            return bin_c,np.dot(mbb_inv,binnedPower)
+            return bin_c, np.dot(mbb_inv,binnedPower)
     else:
         if mcm_inv is not None:
             unbin_vec=[]
-            mcm_inv=so_mcm.coupling_dict_to_array(mcm_inv)
+            mcm_inv = so_mcm.coupling_dict_to_array(mcm_inv)
             for f in spectra:
-                unbin_vec=np.append(unbin_vec,cl[f][2:lmax])
-            cl=vec2spec_dict(lmax-2,np.dot(mcm_inv,unbin_vec),spectra)
-            l=np.arange(2,lmax)
+                unbin_vec = np.append(unbin_vec, cl[f][2:lmax])
+            cl = vec2spec_dict(lmax-2, np.dot(mcm_inv,unbin_vec), spectra)
+            l = np.arange(2,lmax)
 
         vec=[]
         for f in spectra:
@@ -133,17 +131,16 @@ def bin_spectra(l,cl,binning_file,lmax,type,spectra=None,mbb_inv=None,mcm_inv=No
                 loc = np.where((l >= bin_lo[ibin]) & (l <= bin_hi[ibin]))
                 binnedPower[ibin] = (cl[f][loc]*fac[loc]).mean()
             
-            vec=np.append(vec,binnedPower)
+            vec = np.append(vec, binnedPower)
         if mbb_inv is None:
-            return bin_c,vec2spec_dict(n_bins,vec,spectra)
+            return bin_c, vec2spec_dict(n_bins, vec, spectra)
         else:
-            mbb_inv=so_mcm.coupling_dict_to_array(mbb_inv)
-            return bin_c,vec2spec_dict(n_bins,np.dot(mbb_inv,vec), spectra)
+            mbb_inv = so_mcm.coupling_dict_to_array(mbb_inv)
+            return bin_c, vec2spec_dict(n_bins,np.dot(mbb_inv,vec), spectra)
 
-    return(binCent,binnedPower)
+    return binCent, binnedPower
 
-def vec2spec_dict(n_bins,vec,spectra):
-    
+def vec2spec_dict(n_bins, vec, spectra):
     """Take a vector of power spectra and return a power spectra dictionnary.
     vec should be of the form [spectra[0], spectra[1], ... ].
     For example [cl_TT,cl_TE,cl_ET, ...., cl_BB]
@@ -159,14 +156,13 @@ def vec2spec_dict(n_bins,vec,spectra):
       ['TT','TE','TB','ET','BT','EE','EB','BE','BB']
     """
     
-    dict={}
+    dict = {}
     for c,f in enumerate(spectra):
-        dict[f]=vec[c*n_bins:(c+1)*n_bins]
+        dict[f] = vec[c*n_bins:(c+1)*n_bins]
     return dict
 
 
-def write_ps(file_name,l,ps,type,spectra=None):
-    
+def write_ps(file_name, l, ps, type, spectra=None):
     """Write down the power spectra to disk.
     
     Parameters
@@ -184,24 +180,23 @@ def write_ps(file_name,l,ps,type,spectra=None):
     """
 
     if spectra is None:
-        ps_list=[ps]
-        ps_list[0:0]=[l]
-        str='l'
-        str+=' %s'%type
-        ps_list=np.array(ps_list)
-        np.savetxt(file_name, np.transpose(ps_list),header=str)
+        ps_list = [ps]
+        ps_list[0:0] = [l]
+        str = "l"
+        str += " %s"%type
+        ps_list = np.array(ps_list)
+        np.savetxt(file_name, np.transpose(ps_list), header=str)
     else:
-        ps_list=[ps[f] for f in spectra]
-        ps_list[0:0]= [l]
-        str='l'
+        ps_list = [ps[f] for f in spectra]
+        ps_list[0:0] = [l]
+        str = "l"
         for l1 in spectra:
-            str+=' %s_%s'%(type,l1)
-        ps_list=np.array(ps_list)
-        np.savetxt(file_name, np.transpose(ps_list),header=str)
+            str += " %s_%s"%(type, l1)
+        ps_list = np.array(ps_list)
+        np.savetxt(file_name, np.transpose(ps_list), header=str)
 
 
-def read_ps(file_name,spectra=None):
-    
+def read_ps(file_name, spectra=None):
     """Read the power spectra.
         
     Parameters
@@ -218,20 +213,19 @@ def read_ps(file_name,spectra=None):
     array or a dictionnary of power spectra (if spectra is not None).
     """
     
-    data=np.loadtxt(file_name)
+    data = np.loadtxt(file_name)
     if spectra is None:
-        return data[:,0],data[:,1]
+        return data[:,0], data[:,1]
     else:
-        ps={}
-        l=data[:,0]
+        ps = {}
+        l = data[:,0]
         for c,f in enumerate(spectra):
-            ps[f]=data[:,c+1]
-        return(l,ps)
+            ps[f] = data[:,c+1]
+        return l,ps
 
 
 
-def write_ps_hdf5(file,spec_name,l,ps,spectra=None):
-    
+def write_ps_hdf5(file, spec_name, l, ps, spectra=None):
     """Write down the power spectra in a hdf5 file.
         
     Parameters
@@ -249,22 +243,21 @@ def write_ps_hdf5(file,spec_name,l,ps,spectra=None):
     """
 
     def array_from_dict(l,ps,spectra=None):
-        array=[]
-        array+=[l]
-        if spectra==None:
-            array+=[ps]
+        array = []
+        array += [l]
+        if spectra == None:
+            array += [ps]
         else:
             for spec in spectra:
-                array+=[ps[spec]]
-        return(array)
+                array += [ps[spec]]
+        return array
     
-    group=file.create_group(spec_name)
-    array=array_from_dict(l,ps,spectra=spectra)
-    group.create_dataset(name='data',data=array,dtype='float')
+    group = file.create_group(spec_name)
+    array = array_from_dict(l, ps, spectra=spectra)
+    group.create_dataset(name="data", data=array, dtype="float")
 
 
 def read_ps_hdf5(file,spec_name,spectra=None):
-
     """Read the power spectra in a hdf5 file.
         
     Parameters
@@ -284,16 +277,16 @@ def read_ps_hdf5(file,spec_name,spectra=None):
 
     """
     
-    spec=file[spec_name]
-    data=np.array(spec['data']).T
+    spec = file[spec_name]
+    data = np.array(spec["data"]).T
     
-    l=data[:,0]
-    if spectra==None:
-        ps=data[:,1]
+    l = data[:,0]
+    if spectra == None:
+        ps = data[:,1]
     else:
-        ps={}
+        ps = {}
         for count,spec in enumerate(spectra):
-            ps[spec]=data[:,count+1]
+            ps[spec] = data[:,count+1]
 
     return l,ps
 
