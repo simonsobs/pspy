2de84438e75a7bdec76c01e281ef2dd855374513
Xavier Garrido <xavier.garrido@gmail.com>
2020-01-10 13:53:19 +0100

    isortify

--- pspy/__init__.py
+++ pspy/__init__.py
@@ -1,18 +1,10 @@
 from __future__ import absolute_import, print_function
-from pspy import so_config,\
-    so_map, \
-    so_cov, \
-    so_mcm, \
-    so_spectra, \
-    so_window, \
-    sph_tools,\
-    pspy_utils,\
-    so_map_preprocessing,\
-    mcm_fortran,\
-    so_dict,\
-    so_misc,\
-    so_mpi
+
+from pspy import (mcm_fortran, pspy_utils, so_config, so_cov, so_dict, so_map,
+                  so_map_preprocessing, so_mcm, so_misc, so_mpi, so_spectra,
+                  so_window, sph_tools)
 
 from ._version import get_versions
+
 __version__ = get_versions()['version']
 del get_versions
