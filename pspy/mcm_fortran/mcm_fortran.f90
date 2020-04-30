ee42c2535f122d9488c75a6e1a0ea74aaf3a0915
Thibaut Louis <thibautlouis@mbp-de-thibaut.home>
2020-04-30 11:36:32 +0200

    format pure B modes

--- pspy/mcm_fortran/mcm_fortran.f90
+++ pspy/mcm_fortran/mcm_fortran.f90
@@ -15,7 +15,7 @@ subroutine calc_coupling_elem_spin0(wcl, l1, l2, nlmax, elem)
     elem = 0
     do l3 = wlmin, wlmax
         i = l3 - wlmin + 1
-        elem = elem + wcl(l3+1)*thrcof0(i)**2
+        elem = elem + wcl(l3+1) * thrcof0(i)**2
     end do
 end subroutine
 
@@ -35,13 +35,58 @@ subroutine calc_coupling_elem_spin0and2(wcl_00, wcl_02, wcl_20, wcl_22, l1, l2, 
     elems = 0
     do l3 = wlmin, wlmax
         i = l3 - wlmin + 1
-        elems(1) = elems(1) + wcl_00(l3+1)*thrcof0(i)**2
-        elems(2) = elems(2) + wcl_02(l3+1)*thrcof0(i)*thrcof1(i)
-        elems(3) = elems(3) + wcl_20(l3+1)*thrcof0(i)*thrcof1(i)
-        elems(4) = elems(4) + wcl_22(l3+1)*thrcof1(i)**2*(1+(-1)**(l1+l2+l3))/2
-        elems(5) = elems(5) + wcl_22(l3+1)*thrcof1(i)**2*(1-(-1)**(l1+l2+l3))/2
+        elems(1) = elems(1) + wcl_00(l3+1) * thrcof0(i)**2
+        elems(2) = elems(2) + wcl_02(l3+1) * thrcof0(i) * thrcof1(i)
+        elems(3) = elems(3) + wcl_20(l3+1) * thrcof0(i) * thrcof1(i)
+        elems(4) = elems(4) + wcl_22(l3+1) * thrcof1(i)**2 * (1 + (-1)**(l1 + l2 + l3)) / 2
+        elems(5) = elems(5) + wcl_22(l3+1) * thrcof1(i)**2 * (1 - (-1)**(l1 + l2 + l3)) / 2
     end do
 end subroutine
+
+subroutine calc_coupling_elem_spin0and2_pure(wcl_00, wcl_02, wcl_20, wcl_22, l1, l2, nlmax, elems)
+    implicit none
+    integer, intent(in)   :: l1, l2, nlmax
+    real(8), intent(in)   :: wcl_00(:), wcl_02(:), wcl_20(:), wcl_22(:)
+    real(8), intent(inout):: elems(5)
+    real(8) :: thrcof0(2*(nlmax+1)), thrcofa(2*(nlmax+1)), thrcofb(2*(nlmax+1)), thrcofc(2*(nlmax+1)), l1f, l2f, fac_b, fac_c, combin
+    integer :: info, l3, wlmin1, wlmax1, i1, wlmin2, wlmax2, i2, wlmin3, wlmax3, i3
+
+    call drc3jj(dble(l1), dble(l2), 0d0, 0d0, l1f, l2f, thrcof0, size(thrcof0), info)
+    call drc3jj(dble(l1), dble(l2), -2d0, 2d0, l1f, l2f, thrcofa, size(thrcofa), info)
+    wlmin1 = int(l1f)
+    wlmax1 = min(nlmax + 1, int(l2f))
+    call drc3jj(dble(l1), dble(l2), -2d0, 1d0, l1f, l2f, thrcofb, size(thrcofb), info)
+    wlmin2 = int(l1f)
+    wlmax2 = min(nlmax + 1, int(l2f))
+    call drc3jj(dble(l1), dble(l2), -2d0, 0d0, l1f, l2f, thrcofc, size(thrcofc), info)
+    wlmin3 = int(l1f)
+    wlmax3 = min(nlmax + 1, int(l2f))
+    do l3 = wlmin1, wlmax1
+        i1   = l3 - wlmin1 + 1
+        i2   = l3 - wlmin2 + 1
+        i3   = l3 - wlmin3 + 1
+        fac_b = 2 * dsqrt((l3 + 1d0) * l3 / ((l2 - 1d0) * (l2 + 2d0)))
+        fac_c = dsqrt((l3 + 2d0) * (l3 + 1d0) * l3 * (l3 - 1d0) / ((l2 + 2d0) * (l2 + 1d0) * l2 * (l2 - 1d0)))
+
+        if (i2 < 0) then
+            fac_b = 0d0
+        end if
+
+        if (i3 < 0) then
+            fac_c = 0d0
+        end if
+
+        combin = thrcofa(i1) + fac_b * thrcofb(i2) + fac_c * thrcofc(i3)
+        elems(1) = elems(1) + (wcl_00(l3+1) * thrcof0(i1)**2d0)
+        elems(2) = elems(2) + (wcl_02(l3+1) * thrcof0(i1) * combin)
+        elems(3) = elems(3) + (wcl_20(l3+1) * thrcof0(i1) * combin)
+        elems(4) = elems(4) + (wcl_22(l3+1) * combin**2 * (1 + (-1)**(l1 + l2 + l3)) / 2)
+        elems(5) = elems(5) + (wcl_22(l3+1) * combin**2 * (1 - (-1)**(l1 + l2 + l3)) / 2)
+    
+    end do
+end subroutine
+
+ 
 
 subroutine calc_coupling_spin0(wcl, l_exact, l_band, l_toeplitz, coupling)
     implicit none
@@ -123,58 +168,20 @@ subroutine calc_coupling_spin0and2(wcl_00, wcl_02, wcl_20, wcl_22, l_exact, l_ba
 end subroutine
 
 
-subroutine calc_mcm_spin0and2_pure(wcl_00, wcl_02, wcl_20, wcl_22, mcm_array)
+subroutine calc_mcm_spin0and2_pure(wcl_00, wcl_02, wcl_20, wcl_22, coupling_array)
     implicit none
     real(8), intent(in)    :: wcl_00(:), wcl_02(:), wcl_20(:), wcl_22(:)
-    real(8), intent(inout) :: mcm_array(:,:,:)
-    real(8), parameter     :: pi = 3.14159265358979323846264d0
-    integer :: l1, l2, l3, info, nlmax, lmin1, lmax1, i1, lmin2, lmax2, i2, lmin3, lmax3, i3
-    real(8) :: l1f(2),fac_b,fac_c,combin
-    real(8) :: thrcof0(2*size(mcm_array,1)), thrcofa(2*size(mcm_array,1)), thrcofb(2*size(mcm_array,1)), thrcofc(2*size(mcm_array,1))
-    nlmax = size(mcm_array,1)-1
-    !$omp parallel do private(l3,l2,l1,info,l1f,thrcof0,thrcofa,thrcofb,thrcofc,lmin1,lmax1,i1,lmin2,lmax2,i2,lmin3,lmax3,i3,fac_b,fac_c,combin) schedule(dynamic)
+    real(8), intent(inout) :: coupling_array(:,:,:)
+    integer :: l1, l2, nlmax
+    nlmax = size(coupling_array,1)-1
+    !$omp parallel do private(l2,l1) schedule(dynamic)
     do l1 = 2, nlmax
         do l2 = 2, nlmax
-            call drc3jj(dble(l1), dble(l2), 0d0, 0d0, l1f(1), l1f(2), thrcof0, size(thrcof0), info)
-            call drc3jj(dble(l1), dble(l2), -2d0, 2d0, l1f(1), l1f(2), thrcofa, size(thrcofa), info)
-            lmin1=INT(l1f(1)) 
-            lmax1=MIN(nlmax+1,INT(l1f(2)))
-            call drc3jj(dble(l1),dble(l2),-2d0, 1d0, l1f(1), l1f(2), thrcofb, size(thrcofb), info)
-            lmin2=INT(l1f(1))
-            lmax2=MIN(nlmax+1,INT(l1f(2)))
-            call drc3jj(dble(l1),dble(l2),-2d0,0d0,l1f(1),l1f(2),thrcofc, size(thrcofc),info)
-            lmin3=INT(l1f(1))
-            lmax3=MIN(nlmax+1,INT(l1f(2)))
-            do l3=lmin1,lmax1
-                i1   = l3-lmin1+1
-                i2   = l3-lmin2+1
-                i3   = l3-lmin3+1
-
-                fac_b=2*dsqrt((l3+1d0)*l3/((l2-1d0)*(l2+2d0)))
-                fac_c=dsqrt((l3+2d0)*(l3+1d0)*l3*(l3-1d0)/((l2+2d0)*(l2+1d0)*l2*(l2-1d0)))
-
-                if (i2 < 0) then
-                    fac_b=0d0
-                end if
-
-                if (i3 < 0) then
-                    fac_c=0d0
-                end if
-
-                combin=thrcofa(i1) + fac_b*thrcofb(i2) + fac_c*thrcofc(i3)
-
-                mcm_array(l1-1,l2-1,1) = mcm_array(l1-1,l2-1,1) + (wcl_00(l3+1)*thrcof0(i1)**2d0)
-                mcm_array(l1-1,l2-1,2) = mcm_array(l1-1,l2-1,2) + (wcl_02(l3+1)*thrcof0(i1)*combin)
-                mcm_array(l1-1,l2-1,3) = mcm_array(l1-1,l2-1,3) + (wcl_20(l3+1)*thrcof0(i1)*combin)
-                mcm_array(l1-1,l2-1,4) = mcm_array(l1-1,l2-1,4) + (wcl_22(l3+1)*combin**2*(1+(-1)**(l1+l2+l3))/2)
-                mcm_array(l1-1,l2-1,5) = mcm_array(l1-1,l2-1,5) + (wcl_22(l3+1)*combin**2*(1-(-1)**(l1+l2+l3))/2)
-        
-            end do
+            call calc_coupling_elem_spin0and2_pure(wcl_00, wcl_02, wcl_20, wcl_22, l1, l2, nlmax, coupling_array(l1-1,l2-1,:))
         end do
     end do
 
 end subroutine
-
 
 
 subroutine bin_mcm(mcm, binLo, binHi, binsize, mbb, doDl)
