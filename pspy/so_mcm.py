0791b0e3c9ec3329250ad5c08a9a85f615a1c65f
Thibaut Louis <thibautlouis@mbp-de-thibaut.home>
2020-05-02 08:09:11 +0200

    timeit

--- pspy/so_mcm.py
+++ pspy/so_mcm.py
@@ -235,7 +235,6 @@ def mcm_and_bbl_spin0and2(win1,
         if l_toep is None: l_toep = lmax
         if l_band is None: l_band = lmax
         if l_exact is None: l_exact = lmax
-        import time
         t=time.time()
         mcm_fortran.calc_coupling_spin0and2(wcl["00"],
                                             wcl["02"],
@@ -253,7 +252,8 @@ def mcm_and_bbl_spin0and2(win1,
             print(time.time()-t)
 
             mcm_fortran.fill_upper(mcm[id_mcm].T)
-            print(time.time()-t)
+            
+        print(time.time()-t)
 
             #mcm[id_mcm] = mcm[id_mcm] + mcm[id_mcm].T - np.diag(np.diag(mcm[id_mcm]))
     else:
