a8e6ada51418c5c782228df3914026603eeba88e
Thibaut Louis <thibautlouis@MacBook-Pro-de-Thibaut.local>
2020-10-12 16:08:46 +0200

    add options

--- pspy/so_map_preprocessing.py
+++ pspy/so_map_preprocessing.py
@@ -1,7 +1,7 @@
 import numpy as np, pylab as plt
 from pspy import flat_tools
 
-def kspace_filter(map, vk_mask, hk_mask, window=None):
+def kspace_filter(map, vk_mask=None, hk_mask=None, window=None):
 
     filtered_map = map.copy()
 
@@ -9,17 +9,23 @@ def kspace_filter(map, vk_mask, hk_mask, window=None):
     if window is not None:
         ft = flat_tools.get_ffts(map,window)
 
-    id_vk = np.where((ft.lx > vk_mask[0]) & (ft.lx < vk_mask[1]))
-    id_hk = np.where((ft.lx > hk_mask[0]) & (ft.lx < hk_mask[1]))
+    if vk_mask is not None:
+        id_vk = np.where((ft.lx > vk_mask[0]) & (ft.lx < vk_mask[1]))
+    if hk_mask is not None:
+        id_hk = np.where((ft.lx > hk_mask[0]) & (ft.lx < hk_mask[1]))
 
     if map.ncomp == 1:
-        ft.kmap[: , id_vk] = 0.
-        ft.kmap[id_hk , :] = 0.
+        if vk_mask is not None:
+            ft.kmap[: , id_vk] = 0.
+        if hk_mask is not None:
+            ft.kmap[id_hk , :] = 0.
         
     if map.ncomp == 3:
         for i in range(3):
-            ft.kmap[i, : , id_vk] = 0.
-            ft.kmap[i, id_hk , :] = 0.
+            if vk_mask is not None:
+                ft.kmap[i, : , id_vk] = 0.
+            if hk_mask is not None:
+                ft.kmap[i, id_hk , :] = 0.
 
     filtered_map.data[:] = ft.map_from_fft()
     return filtered_map
