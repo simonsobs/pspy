06bf1174132e70b187d640595db80621a3e98a00
Xavier Garrido <xavier.garrido@gmail.com>
2023-05-16 13:42:37 +0200

    Remove duplicated and obsolete code

--- pspy/__init__.py
+++ pspy/__init__.py
@@ -4,10 +4,5 @@ from pspy import (mcm_fortran, pspy_utils, so_config, so_cov, so_dict, so_map,
                   so_map_preprocessing, so_mcm, so_misc, so_mpi, so_spectra,
                   so_window, sph_tools, flat_tools)
 
-from ._version import get_versions
-
-__version__ = get_versions()['version']
-del get_versions
-
 from . import _version
 __version__ = _version.get_versions()['version']
